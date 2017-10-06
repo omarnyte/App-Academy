6a746d1ff0a9497b254bd4d21b445bc021b31ecd
Omar <omar.dlsg@gmail.com>
2017-10-05 18:13:38 -0700

    End of W1D4

--- minesweeper/board.rb
+++ minesweeper/board.rb
@@ -19,7 +19,8 @@ class Board
 
     grid.each_index do |row|
       grid[row].each_index do |col|
-        grid[row][col] = Tile.new(bomb: false, reveal: false) if grid[row][col].nil?
+        val = nearby_bombs([row,col])
+        grid[row][col] = Tile.new(bomb: false, value: val) if grid[row][col].nil?
       end
     end
   end
@@ -32,7 +33,34 @@ class Board
       x = (0..8).to_a.sample
       y = (0..8).to_a.sample
     end
-    grid[x][y] = Tile.new(bomb: true, reveal: false)
+    grid[x][y] = Tile.new(bomb: true, value: 0)
+  end
+
+  def render
+    puts "  #{(0..8).to_a.join("  ")}"
+
+    @grid.each_index do |row|
+      print row
+      @grid[row].each do |char|
+        print " #{ char.to_s } "
+      end
+      puts
+    end
+  end
+
+  def nearby_bombs(pos)  #### DID NOT FINISH THIS!
+    count = 0
+    x, y = pos
+    @grid[y-1].each.with_index do |abv_tile, idx|
+      count += 1 if abv_tile.bomb && idx.between?(x-1, x+1)
+    end
+    @grid[y+1].each.with_index do |below_tile, idx|
+      count += 1 if below_tile.bomb && idx.between?(x-1, x+1)
+    end
+    @grid[y].each.with_index do |adj_tile, idx|
+      count += 1 if adj_tile.bomb && idx == x-1 || x+1
+    end
+    count
   end
 
   def [](pos)
--- minesweeper/tile.rb
+++ minesweeper/tile.rb
@@ -1,9 +1,40 @@
 class Tile
+attr_reader :bomb, :flag, :value
+
+  attr_accessor :reveal, :bomb
 
   def initialize(options = {})
-    defaults = { bomb: false, reveal: false }
+    defaults = { bomb: false, value: 0 }
     options = defaults.merge(options)
     @bomb = options[:bomb]
-    @reveal = options[:reveal]
+    @reveal = false
+    @flag = false
+    @value = options[:value]
   end
+
+  def to_s
+    if @reveal
+      if @bomb
+        "!"
+      else
+        @value.to_s if @value > 0
+        "-" if @value == 0
+      end
+    else
+      if @flag
+        "F"
+      else
+        "*"
+      end
+    end
+  end
+
+  def show_tile
+    @reveal = true
+  end
+
+  def flag_tile
+    @flag = true
+  end
+
 end
