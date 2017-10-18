5dc4e6d65caafe126c9ecc4cc5dc7b002aab8d43
Omar <omar.dlsg@gmail.com>
2017-10-18 13:44:24 -0700

    Finished associations

--- Associations/app/models/course.rb
+++ Associations/app/models/course.rb
@@ -3,4 +3,14 @@ class Course < ApplicationRecord
   class_name: :User,
   foreign_key: :student_id,
   primary_key: :id
+
+  belongs_to :instructor,
+  class_name: :User,
+  foreign_key: :instructor_id,
+  primary_key: :id
+
+  belongs_to :prerequisite,
+  class_name: :Course,
+  foreign_key: :prereq_id,
+  primary_key: :id 
 end
--- Associations/app/models/user.rb
+++ Associations/app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   class_name: :Course,
   foreign_key: :course_id,
   primary_key: :id
+
+  has_many :enrollments,
+  class_name: :Enrollment,
+  foreign_key: :student_id,
+  primary_key: :id
+
+  has_many :enrolled_courses,
+  through: :enrollments,
+  source: :course
 end
