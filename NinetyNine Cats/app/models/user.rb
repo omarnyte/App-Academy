7bcac4fd7577c7a6a54c1cbe5874231af8c38b6f
omar <omar.dlsg@gmail.com>
2017-10-26 17:57:24 -0700

    Finishes User authentication

--- NinetyNine Cats/app/models/user.rb
+++ NinetyNine Cats/app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
   validates :username, :session_token, presence: true, uniqueness: true
   validates :password_digest, presence: true
+  validates :password, length: { minimum: 6, allow_nil: true }
 
-  after_initialize :reset_session_token!
+  after_initialize :ensure_session_token!
 
   attr_reader :password
 
@@ -18,6 +19,13 @@ class User < ApplicationRecord
     self.save
     self.session_token
   end
+
+  def ensure_session_token!
+    unless self.session_token
+      self.session_token = SecureRandom::urlsafe_base64
+    end 
+  end
+
 
   def password=(password)
     @password = password
