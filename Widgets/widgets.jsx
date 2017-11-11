2ebee6764133b0b18a58e54fd8ff271e9185645d
App Academy Student <student@appacademy.io>
2017-11-10 16:57:28 -0800

    Adds clock and tab widgets

---
+++ Widgets/widgets.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Widget from "./frontend/widget"; 
+
+document.addEventListener("DOMContentLoaded", () => {
+  ReactDOM.render(<Widget />, document.getElementById("root"));
+});
