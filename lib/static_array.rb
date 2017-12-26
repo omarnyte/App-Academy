b3a0bb88d0782bfcce87638eeb01948f758bef4c
Omar <omar.dlsg@gmail.com>
2017-12-26 14:35:10 -0800

    initial commit

---
+++ lib/static_array.rb
@@ -0,0 +1,20 @@
+# This class just dumbs down a regular Array to be statically sized.
+class StaticArray
+  attr_reader :stat_arr
+  def initialize(length)
+
+  end
+
+  # O(1)
+  def [](index)
+
+  end
+
+  # O(1)
+  def []=(index, value)
+    
+  end
+
+  protected
+  attr_accessor :store
+end
