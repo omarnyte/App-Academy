0019212e672e1a6ed9a5fe552afacd0fa85503dc
Omar <omar.dlsg@gmail.com>
2018-01-08 18:16:37 -0800

    Add lecture notes

--- Binary Search Tree/lib/bst_practical.rb
+++ Binary Search Tree/lib/bst_practical.rb
@@ -20,3 +20,19 @@ def kth_largest(tree_node, k)
   array = bst.in_order_traversal
   return bst.find(array[array.length - k])
 end
+
+# In Order Iterative 
+def inorder_iter(tree_node)
+  stack = []
+  current = tree_node 
+  until current.nil ?? stack.empty? 
+    if current 
+      stack << current 
+      current = current.left 
+    else
+      top_node = stack.pop
+      p top_node.value 
+      curent = top_node.right 
+    end 
+  end 
+end 
